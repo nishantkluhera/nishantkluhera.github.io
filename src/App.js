082b0fb850653213c656bd0c8e13d10957eb3b89
Nishant Luhera <125179479+nishantkluhera@users.noreply.github.com>
2024-01-19 05:27:19 +0530

    hopefully it works this time

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Skills />
+
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+import python from "../assets/python.png";
+import java from "../assets/Java.png";
+import javascript from "../assets/javascript.png";
+import reactImage from "../assets/react.png";
+import nextjs from "../assets/nextjs.png";
+import tensorflow from "../assets/tensorflow.png";
+import github from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
+import cpp from "../assets/C++.png";
+
+const Skills = () => {
+  const techs = [
+    {
+      id: 1,
+      src: cpp,
+      title: "C++",
+      style: "shadow-blue-800", 
+    },
+    {
+      id: 2,
+      src: python,
+      title: "Python",
+      style: "shadow-yellow-800",
+    },
+    {
+      id: 3,
+      src: java,
+      title: "Java",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 4,
+      src: tensorflow,
+      title: "Tensorflow",
+      style: "shadow-orange-400",
+    },
+    {
+      id: 5,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 6,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 7,
+      src: nextjs,
+      title: "Next JS",
+      style: "shadow-white",
+    },
+    {
+      id: 8,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-pink-400",
+    },
+    
+    {
+      id: 9,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-400",
+    },
+  ];
+
+  return (
+    <div
+      name="Skills"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Skills
+          </p>
+          <p className="py-6">These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {techs.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
